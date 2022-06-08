f690e8b3d94260a0c9eb97cf785caa645c6fe07b
Sai Kumar <sai@vitwit.com>
2022-06-08 13:25:38 +0530

    add upgrade handler

--- app/app.go
+++ app/app.go
@@ -409,6 +409,7 @@ func NewEthermintApp(
 	)
 	app.FeeGrantKeeper = feegrantkeeper.NewKeeper(appCodec, keys[feegrant.StoreKey], app.AccountKeeper)
 	app.UpgradeKeeper = upgradekeeper.NewKeeper(skipUpgradeHeights, keys[upgradetypes.StoreKey], appCodec, homePath, app.BaseApp, authtypes.NewModuleAddress(govtypes.ModuleName).String())
+	app.registerUpgrade()
 
 	// register the staking hooks
 	// NOTE: stakingKeeper above is passed by reference, so that it will contain these hooks
---
+++ app/upgrade.go
@@ -0,0 +1,82 @@
+package app
+
+import (
+	"path/filepath"
+
+	v1rootmultistore "github.com/cosmos/cosmos-sdk/store/rootmulti"
+	"github.com/cosmos/cosmos-sdk/store/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/v2alpha1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tdbm "github.com/tendermint/tm-db"
+)
+
+func (app *EthermintApp) registerUpgradeHandlers(keys map[string]*storetypes.KVStoreKey) {
+	// mount the v1 store
+	rootDir := "/home/vitwit/.chibaclonkd"
+	dataDir := filepath.Join(rootDir, "data")
+
+	db, err := tdbm.NewGoLevelDB("application", dataDir)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	cms := v1rootmultistore.NewStore(db, log.NewNopLogger())
+	for _, key := range keys {
+		cms.MountStoreWithDB(key, types.StoreTypeIAVL, nil)
+	}
+
+	// mainnet upgrade handler
+	const upgradeName = "smt-upgrade"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// store upgrade from iavl to smt
+		// _ , err := v2multi.MigrateFromV1(cms,app)
+		// if err != nil {
+		// 	return nil, err
+		// }
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+}
+
+func (app *EthermintApp) registerUpgrade() {
+	// mainnet upgrade handler
+	const upgradeName = "smt-upgrade"
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
+		// store upgrade from iavl to smt
+		// _ , err := v2multi.MigrateFromV1(cms,app)
+		// if err != nil {
+		// 	return nil, err
+		// }=
+
+		fromVM := map[string]uint64{
+			"auth":         2,
+			"authz":        1,
+			"bank":         2,
+			"capability":   1,
+			"crisis":       1,
+			"distribution": 2,
+			"evidence":     1,
+			"feegrant":     1,
+			"gov":          2,
+			"mint":         1,
+			"params":       1,
+			"slashing":     2,
+			"staking":      2,
+			"upgrade":      1,
+			"vesting":      1,
+			"ibc":          2,
+			"genutil":      1,
+			"transfer":     1,
+		}
+
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
+}
