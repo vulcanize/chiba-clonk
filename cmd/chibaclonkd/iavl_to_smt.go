c1d31d53fca97e60cd514042506308def962dc70
Sai Kumar <sai@vitwit.com>
2022-06-14 17:34:24 +0530

    WIP : refactor the iavl-to-smt cli cmd

--- cmd/chibaclonkd/iavl_to_smt.go
+++ cmd/chibaclonkd/iavl_to_smt.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path"
 
@@ -10,42 +9,49 @@ import (
 	"github.com/cosmos/cosmos-sdk/store/types"
 	storetypes "github.com/cosmos/cosmos-sdk/store/v2alpha1"
 	v2multistore "github.com/cosmos/cosmos-sdk/store/v2alpha1/multi"
-	"github.com/google/orderedcode"
+	"github.com/cosmos/cosmos-sdk/version"
 
 	dbm "github.com/cosmos/cosmos-sdk/db"
-	"github.com/gogo/protobuf/proto"
 	"github.com/spf13/cobra"
 	tmlog "github.com/tendermint/tendermint/libs/log"
-	tmstate "github.com/tendermint/tendermint/proto/tendermint/state"
-	"github.com/tendermint/tendermint/version"
 	tmdb "github.com/tendermint/tm-db"
 )
 
+var logger tmlog.Logger
+
+func init() {
+	logger, _ = tmlog.NewDefaultLogger("plain", "info", false)
+	logger = logger.With("module", "state_migration")
+}
+
+// StateMigrationFromIAVLtoSMT cli cmd for migrate the data from iavl to smt and goleveldb to badgerdb
 func StateMigrationFromIAVLtoSMT(keys map[string]*storetypes.KVStoreKey) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "iavl-to-smt [old-data-home-dir] [new-data-home-dir]",
 		Short:   "State migraiton from iavl to smt",
-		Example: fmt.Sprint("%s iavl-to-smt"),
+		Example: fmt.Sprintf("%s iavl-to-smt ~/.chibaclonkd ~/.newchibaclonkd", version.AppName),
 		Args:    cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			home := args[0]
 			if _, err := os.Stat(home); err != nil {
-				log.Fatal(err)
+				logger.Error(err.Error())
+				return err
 			}
 			dataDir := path.Join(home, "data")
 
 			newDir := args[1]
 			if _, err := os.Stat(home); err != nil {
-				log.Fatal(err)
+				logger.Error(err.Error())
+				return err
 			}
 			newdataDir := path.Join(newDir, "data")
 
 			err := iavlToSmt(keys, dataDir, newdataDir)
 			if err != nil {
-				fmt.Println("[X] State migration failed from iavl to smt.")
+				logger.Error(fmt.Sprintf("State migration failed from iavl to smt. Err %v\n", err))
 				return err
 			}
-			fmt.Println("[!] State migration completed from iavl to smt.")
+			logger.Info("State migration is completed from iavl to smt and goleveldb to badgerdb.")
 			return nil
 		},
 	}
@@ -53,33 +59,36 @@ func StateMigrationFromIAVLtoSMT(keys map[string]*storetypes.KVStoreKey) *cobra.
 	return cmd
 }
 
+// iavlToSmt migrate the data from iavl to smt
 func iavlToSmt(keys map[string]*storetypes.KVStoreKey, dataDir, newDataDir string) error {
-	oldLevelDb, err := tmdb.NewGoLevelDB("application", dataDir)
+	v1LevelDB, err := tmdb.NewGoLevelDB("application", dataDir)
 	if err != nil {
 		return err
 	}
-	cms := rootmulti.NewStore(oldLevelDb, tmlog.NewNopLogger())
+	// v1 Store
+	cms := rootmulti.NewStore(v1LevelDB, tmlog.NewNopLogger())
 	// mount the kvStore
 	for _, key := range keys {
-		cms.MountStoreWithDB(key, types.StoreTypeIAVL, oldLevelDb)
+		cms.MountStoreWithDB(key, types.StoreTypeIAVL, v1LevelDB)
 	}
+	logger.Info("v1store init is done.")
 
-	fmt.Println("v1store init is done.")
-	// new store v2
-	// new dir
+	//  v2 new store
 	opts := v2multistore.DefaultStoreParams()
-
 	ndb, err := dbm.NewDB("application", dbm.BadgerDBBackend, newDataDir)
 	if err != nil {
 		return err
 	}
-	fmt.Println("badgerdb initial succeed")
-	lcd, ss, err := v2multistore.MigrateFromV1(cms, ndb, opts)
+	logger.Info("badgerdb init done for application")
+
+	// migrating the data from v1 to v2
+	_, err = v2multistore.MigrateFromV1(cms, ndb, opts)
 	if err != nil {
+		logger.Error(err.Error())
 		return err
 	}
 
-	fmt.Println("Commit hash", string(ss.Commit().Hash))
+	// migrating the data from goleveldb to badgerdb
 	stores := [...]string{
 		"blockstore",
 		"state",
@@ -90,78 +99,51 @@ func iavlToSmt(keys map[string]*storetypes.KVStoreKey, dataDir, newDataDir strin
 	}
 
 	for _, s := range stores {
-		t, err := tmdb.NewGoLevelDB(s, dataDir)
+		// goleveldb init
+		goleveldb, err := tmdb.NewGoLevelDB(s, dataDir)
 		if err != nil {
 			return err
 		}
-		// new badgerdb
-		b, err := dbm.NewDB(s, dbm.BadgerDBBackend, newDataDir)
+		iter, err := goleveldb.Iterator(nil, nil)
 		if err != nil {
 			return err
 		}
-		r := b.ReadWriter()
-		iter, err := t.Iterator(nil, nil)
+
+		// badgerdb init
+		bagerDb, err := dbm.NewDB(s, dbm.BadgerDBBackend, newDataDir)
 		if err != nil {
 			return err
 		}
+
+		// read writer for badgerdb
+		rw := bagerDb.ReadWriter()
+
 		for ; iter.Valid(); iter.Next() {
-			r.Set(iter.Key(), iter.Value())
+			err = rw.Set(iter.Key(), iter.Value())
+			if err != nil {
+				return err
+			}
 		}
-		err = r.Commit()
+
+		// commit the data into badgerdb
+		err = rw.Commit()
 		if err != nil {
 			return err
 		}
-		b.Close()
-		t.Close()
+
+		// close the badgerdb connection
+		err = bagerDb.Close()
+		if err != nil {
+			return err
+		}
+
+		// close the goleveldb connection
+		err = goleveldb.Close()
+		if err != nil {
+			return err
+		}
 	}
 
-	fmt.Println("state tendetmint migration is done.")
-	// access state db
-
-	sdb, err := dbm.NewDB("state", dbm.BadgerDBBackend, newDataDir)
-	if err != nil {
-		return err
-	}
-
-	type Version struct {
-		Consensus version.Consensus ` json:"consensus"`
-		Software  string            ` json:"software"`
-	}
-
-	fmt.Println("state db init done")
-	prefixState := int64(8)
-	stateKey, err := orderedcode.Append(nil, prefixState)
-	if err != nil {
-		panic(err)
-	}
-
-	rat := sdb.ReadWriter()
-	buf, err := rat.Get(stateKey)
-	if err != nil {
-		panic(err)
-	}
-
-	sp := new(tmstate.State)
-	err = proto.Unmarshal(buf, sp)
-	if err != nil {
-		// DATA HAS BEEN CORRUPTED OR THE SPEC HAS CHANGED
-		fmt.Sprintf(`LoadState: Data has been corrupted or its spec has changed:%v\n`, err)
-	}
-
-	fmt.Println("last hash form state ", sp.AppHash)
-	fmt.Print("new hash from badger ", lcd.Hash)
-	sp.AppHash = lcd.Hash
-	b, err := proto.Marshal(sp)
-	if err != nil {
-		panic(err)
-	}
-	err = rat.Set(stateKey, b)
-	if err != nil {
-		panic(err)
-	}
-	err = rat.Commit()
-	if err != nil {
-		panic(err)
-	}
+	logger.Info("state tendetmint migration is done.")
 	return nil
 }
